ca6c38a5f0f2cf5a033d531c9ac0565e58b38017
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-21 17:44:28 -0300

    Exercise 1 solution

--- exercise-1.ts
+++ exercise-1.ts
@@ -1,22 +1,26 @@
 // Given the data, define the interface "User" and use it accordingly.
 
-export type User = unknown;
+export interface User {
+  name: string;
+  age: number;
+  occupation: string;
+}
 
-export const users: unknown[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    }
+export const users: User[] = [
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+  },
 ];
 
-export function logPerson(user: unknown) {
-    console.log(` - ${user.name}, ${user.age}`);
+export function logPerson(user: User) {
+  console.log(` - ${user.name}, ${user.age}`);
 }
 
 console.log('Users:');

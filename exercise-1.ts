251f7db091ea40a01771a62735e3e641a4cbf143
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-21 17:27:23 -0300

    Exercise 01 - Given the data, define the interface "User" and use it accordingly

---
+++ exercise-1.ts
@@ -0,0 +1,23 @@
+// Given the data, define the interface "User" and use it accordingly.
+
+export type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
